d71dcac5682d038a323d571f34eb82972dea629b
unknown <shrikanth.holla@gmail.com>
2019-11-02 16:27:58 +0530

    Fun js commit 0.1

---
+++ src/build/precache-manifest.974159e34b6e488653fbeb0f47eca94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34c1317a82d432b101d91ae8f4c7cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f1c658fe55de2bee99",
+    "url": "/static/css/main.0f5412f4.chunk.css"
+  },
+  {
+    "revision": "9ef714713f02b1346a5e",
+    "url": "/static/js/2.c66188e0.chunk.js"
+  },
+  {
+    "revision": "74f1c658fe55de2bee99",
+    "url": "/static/js/main.efbc6f50.chunk.js"
+  },
+  {
+    "revision": "3691fa66dc8188fbd277",
+    "url": "/static/js/runtime-main.794b9c3f.js"
+  }
+]);
